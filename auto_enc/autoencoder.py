18c58484f8108ada565f6396da5d30df77fa1bb2
ant <athai6@gatech.edu>
2021-02-06 00:01:21 -0500

    remove commented code

--- auto_enc/autoencoder.py
+++ auto_enc/autoencoder.py
@@ -138,41 +138,6 @@ class SegNet(nn.Module):
 
         return up1
 
-    # def init_vgg16_params(self, vgg16):
-    #     blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
-
-    #     ranges = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 29]]
-    #     features = list(vgg16.features.children())
-
-    #     vgg_layers = []
-    #     for _layer in features:
-    #         if isinstance(_layer, nn.Conv2d):
-    #             vgg_layers.append(_layer)
-
-    #     merged_layers = []
-    #     for idx, conv_block in enumerate(blocks):
-    #         if idx < 2:
-    #             units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
-    #         else:
-    #             units = [
-    #                 conv_block.conv1.cbr_unit,
-    #                 conv_block.conv2.cbr_unit,
-    #                 conv_block.conv3.cbr_unit,
-    #             ]
-    #         for _unit in units:
-    #             for _layer in _unit:
-    #                 if isinstance(_layer, nn.Conv2d):
-    #                     merged_layers.append(_layer)
-
-    #     assert len(vgg_layers) == len(merged_layers)
-
-    #     for l1, l2 in zip(vgg_layers, merged_layers):
-    #         if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-    #             assert l1.weight.size() == l2.weight.size()
-    #             assert l1.bias.size() == l2.bias.size()
-    #             l2.weight.data = l1.weight.data
-    #             l2.bias.data = l1.bias.data
-
     def increment_classes(self, new_classes):
         '''
         Add new output nodes when new classes are seen and make changes to
